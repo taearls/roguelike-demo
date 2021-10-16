b765904c1f2c6e81b3a02f80d2fc83d8763fcc72
Tyler Earls <tyler.a.earls@gmail.com>
2021-10-16 13:08:11 -0500

    add monsters, finish 2.5

--- src/player.rs
+++ src/player.rs
@@ -1,5 +1,5 @@
-use super::{Map, Player, Position, State, TileType, Viewshed};
-use rltk::{Rltk, VirtualKeyCode};
+use super::{Map, Player, Position, RunState, State, TileType, Viewshed};
+use rltk::{Rltk, VirtualKeyCode, Point};
 use specs::prelude::*;
 use std::cmp::{max, min};
 
@@ -14,16 +14,19 @@ pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
         if map.tiles[destination_idx] != TileType::Wall {
             pos.x = min(79, max(0, pos.x + delta_x));
             pos.y = min(49, max(0, pos.y + delta_y));
+            let mut ppos = ecs.write_resource::<Point>();
+            ppos.x = pos.x;
+            ppos.y = pos.y;
 
             viewshed.dirty = true;
         }
     }
 }
 
-pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
+pub fn player_input(gs: &mut State, ctx: &mut Rltk) -> RunState {
     // Player movement
     match ctx.key {
-        None => {} // Nothing happened
+        None => return RunState::Paused, // Nothing happened
         Some(key) => match key {
             VirtualKeyCode::Left | VirtualKeyCode::Numpad4 | VirtualKeyCode::H => {
                 try_move_player(-1, 0, &mut gs.ecs)
@@ -41,7 +44,8 @@ pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
                 try_move_player(0, 1, &mut gs.ecs)
             }
 
-            _ => {}
+            _ => return RunState::Paused,
         },
     }
+    RunState::Running
 }
