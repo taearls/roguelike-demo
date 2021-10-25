f57bf84f86e2f0807911fb25bbc0baedc13e9383
Tyler Earls <tyler.a.earls@gmail.com>
2021-10-25 01:35:00 -0500

    add render order

--- src/main.rs
+++ src/main.rs
@@ -136,11 +136,13 @@ impl GameState for State {
 
         draw_map(&self.ecs, ctx);
 
+        let map = self.ecs.fetch::<Map>();
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
-        let map = self.ecs.fetch::<Map>();
 
-        for (pos, render) in (&positions, &renderables).join() {
+        let mut data = (&positions, &renderables).join().collect::<Vec<_>>();
+        data.sort_by(|&a, &b| b.1.render_order.cmp(&a.1.render_order));
+        for (pos, render) in data.iter() {
             let idx = map.xy_idx(pos.x, pos.y);
             if map.visible_tiles[idx] {
                 ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph)
